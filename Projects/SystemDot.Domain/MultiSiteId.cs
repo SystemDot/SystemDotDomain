ccacb5e0e3860f20676ac1efd6240664634806f4
sammosampson <sammosampson@gmail.com>
2015-04-17 20:07:13 +0100

    Added multisite id

---
+++ Projects/SystemDot.Domain/MultiSiteId.cs
@@ -0,0 +1,26 @@
+﻿namespace SystemDot.Domain
+{
+    using SystemDot.Core;
+
+    public abstract class MultiSiteId : Equatable<MultiSiteId>
+    {
+        public string Id { get; private set; }
+        public string SiteId { get; private set; }
+
+        protected MultiSiteId(string id, string siteId)
+        {
+            Id = id;
+            SiteId = siteId;
+        }
+
+        public override bool Equals(MultiSiteId other)
+        {
+            return other.Id == Id && other.SiteId == SiteId;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode().ShiftBitsUp(2) ^ Id.GetHashCode();
+        }
+    }
+}
